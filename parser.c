1684780836614fbcf0baacc21317b735bec86c50
gbafana25 <garethkm25@gmail.com>
2021-07-05 22:38:47 -0500

    parser works

--- parser.c
+++ parser.c
@@ -5,12 +5,13 @@
 
 void get_value(char *s, int pos) {
 	pos += 5;
-	//int start = pos;
-	int len = pos;
-	while(s[len] != '&') {
-		//curr++;
-		//printf("%c\n", s[len]);
-		len++;
+	int len = 0;
+	for(int i = pos; i < strlen(s) - 1; i++) {
+		if(strncmp(&s[i], "&", 1) == 0) {
+			break;
+		} else {
+			len++;
+		}
 	}
 	char *val = malloc(sizeof(char) * len);
 	strncpy(val, s+pos, len);
@@ -26,6 +27,7 @@ void parse_request(char *req) {
 			get_value(req, i);
 		} else if(strncmp(&req[i], "lang=", 5) == 0) {
 			printf("found language\n");
+			get_value(req, i);
 		}
 	}
 
